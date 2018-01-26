8554dda38f27eeb920e578d0e7dd1c60bc772933
Suzanne Millstein <smillst@cs.washington.edu>
2018-01-26 14:51:19 -0800

    Update .travis-build.sh

--- .travis-build.sh
+++ .travis-build.sh
@@ -13,7 +13,7 @@ echo "Should next trigger downstream jobs."
 true
 
 ## Build Checker Framework
-(cd $ROOT && git clone --depth 1 https://github.com/typetools/checker-framework.git)
+(cd $ROOT && git clone --depth 1 -b index-casestudies https://github.com/typetools/checker-framework.git)
 # This also builds annotation-tools and jsr308-langtools
 (cd $ROOT/checker-framework/ && ./.travis-build-without-test.sh downloadjdk)
 export CHECKERFRAMEWORK=$ROOT/checker-framework
